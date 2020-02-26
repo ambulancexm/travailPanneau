29de4f074069913b1bb6ad5890df478d387dc7fc
bulleux <bulleux@localhost.localdomain>
2020-02-26 20:55:22 +0100

    ajout commentaire

--- amperemetre.ino
+++ amperemetre.ino
@@ -1,21 +1,29 @@
 /*
 Measuring Current Using ACS712
 */
-const int analogIn = A0;
-const int voltIn = A1;
-const int luxIn = A2;
+const int ampIn = A0; // input ampere
+const int voltIn = A1; // input volt
+const int luxIn = A2; // input lux
+
+// variable total 
 static long totalSec =0;
 static long totalWatt =0;
+
+// variable de travail Ampere module
 int mVperAmp = 100; // use 100 for 20A Module and 66 for 30A Module
 int RawValue= 0;
 int ACSoffset = 2500; 
 double Voltage = 0;
 double Amps = 0;
 
+
+// variable volt module
 float vout = 0.0;
 float vin = 0.0;
-float R1 = 30000.0; //  
-float R2 = 7500.0; // 
+
+//  resistance pont diviseur
+float R1 = 30000.0; 
+float R2 = 7500.0; 
 int value = 0;
 
 void setup(){ 
@@ -24,20 +32,19 @@ void setup(){
 
 void loop(){
  
+ // lecture ampere
  RawValue = analogRead(analogIn);
- 
  Voltage = (RawValue / 1024.0) * 5000; // Gets you mV
- 
  Amps = ((Voltage - ACSoffset) / mVperAmp);
 
+// lecture volt
+ value = analogRead(voltIn);
+ vout = (value * 5.0) / 1024.0; // see text
+ vin = vout / (R2/(R1+R2)); 
  
- 
-
-  value = analogRead(voltIn);
-   vout = (value * 5.0) / 1024.0; // see text
-   vin = vout / (R2/(R1+R2)); 
- 
+ // increment du total de joule seconde
  totalWatt = totalWatt + (abs(Amps)*vin);
+ // incrément seconde
  totalSec++;
  
  Serial.print("Amps = "); // shows the voltage measured 
